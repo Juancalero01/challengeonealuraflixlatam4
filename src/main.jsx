de8be4a3d4e16aba4eb5d14989c67b676a973443
juancalero <marcelocalero5@gmail.com>
2023-05-06 01:13:31 -0300

    Added: Router provider and main page Browse

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Browse from './pages/Browse';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode></React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RouterProvider
+            router={createBrowserRouter([{ path: '/', Component: Browse }])}
+        />
+    </React.StrictMode>
 );
